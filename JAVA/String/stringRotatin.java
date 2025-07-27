190c8dd870ec50aa5118c828ce96c71228e73c7b
Atharv Dixit <dixitatharv2004@gmail.com>
2025-07-27 12:46:55 +0530

    StringRotation program added

--- JAVA/String/stringRotatin.java
+++ JAVA/String/stringRotatin.java
@@ -1,11 +1,54 @@
 import java.util.*;
 public class stringRotatin{
 
-    static boolean isRotation(String s1 , String s2){
-        
+
+    //this function uses string and it creates string every time while running loop decreasing the efficiency
+    // static boolean isRotation(String s1 , String s2){
+    //     int n = s1.length();
+
+    //     for(int i=0 ; i<n ; i++){
+
+    //         if(s1.equals(s2)){
+    //             return true;
+    //         }
+
+    //         char last = s1.charAt(s1.length()-1);
+    //         s1 = last + s1.substring(0,s1.length()-1);
+    //     }
+
+    //     return false;
+    // }
+
+
+    //In this we use stringbuilder which increases the efficiency
+    static boolean isRotation(String s1,String s2){
+
+        int n = s1.length();
+
+        StringBuilder sb = new StringBuilder(s1);
+
+        for(int i=0 ; i<n ; i++){
+
+            if(sb.toString().equals(s2)){
+                return true;
+            }
+
+            char last = sb.charAt(sb.length()-1);
+            sb.deleteCharAt(sb.length()-1);
+            sb.insert(0, last);
+        }
+
+        return false;
+
     }
 
     public static void main(String[] args) {
-        
+        System.out.print("Enter both strings:");
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        System.out.print(isRotation(s1, s2));
+
     }
 }
